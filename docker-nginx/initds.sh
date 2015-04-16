d0b398000eff04686ddddfcd4d8cddd9095150cd
Kévin Moreau <kevin.moreau@dynamease.com>
2015-04-16 13:39:51 +0200

    (feature) Volume for the conf server nginx

--- docker-nginx/initds.sh
+++ docker-nginx/initds.sh
@@ -6,6 +6,19 @@ NGINX_LOG="/var/log/nginx"
 ERROR_LOG="$NGINX_LOG/error.log"
 ACCESS_LOG="$NGINX_LOG/access.log"
 
+NGINX_PATH="/etc/nginx"
+AVAILABLE="$NGINX_PATH/sites-available"
+ENABLED="$NGINX_PATH/sites-enabled"
+
+rm $ENABLED/*
+
+for file in $(ls $AVAILABLE); do
+    echo $file
+    ln -s $AVAILABLE/$file $ENABLED/$file
+done
+
+ls $ENABLED
+
 service nginx restart
 
 tail -f $ERROR_LOG & tail -f $ACCESS_LOG  
